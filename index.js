2fabc18b9680295736c0a0df202717c5b5858540
ghost <ghostpy91.com>
2025-04-24 14:33:45 -0700

    get route

--- index.js
+++ index.js
@@ -8,15 +8,36 @@ const PORT = 8000;
 app.get("/users", (req, res) => {
   const html = `
     <ul>
-        ${users.map((user) => `<li>${user.first_name}</li>`).join('')}
+        ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
     </ul>
     `;
-    res.send(html)
+  res.send(html);
 });
 
 //REST API
 app.get("/api/users", (req, res) => {
   return res.json(users);
+});
+
+app
+  .route("/api/users/:id")
+  .get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+  })
+  .patch((req, res) => {
+    // edit user with id
+    res.json({ status : 'pending'})
+  })
+  .delete((req, res) => {
+    // delete user with id
+    res.json({ status : 'pending'})
+  });
+
+app.post("/api/users", (req, res) => {
+  // TODO : Create new user
+  return res.json({ status: "pending" });
 });
 
 app.listen(PORT, () => console.log(`Server Started on PORT:${PORT}`));
